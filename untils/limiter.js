2524a1e7ae141c385a87a700f6842e196e035444
Esin Andrew <kubanesin@gmail.com>
2022-05-31 21:03:56 +0300

    fix: поправил limiter

--- untils/limiter.js
+++ untils/limiter.js
@@ -1,7 +1,9 @@
 const rateLimit = require('express-rate-limit');
 
 // Настраиваю защиту
-module.exports.limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 минут
-  max: 100, // лимит запросов с одного ip
+const limiter = rateLimit({
+  windowMs: 90000,
+  max: 100,
 });
+
+module.exports = limiter;
